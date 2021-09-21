58892c5609b8868e7dad4f914cb2da27dc4fe93f
Nabilla <nabillazachra14@gmail.com>
2021-09-21 13:41:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const txt = "Nabilla";
+  const batch = "Fullstack js batch 26!";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Halo, selamat pagi {txt}, selamat mengikuti {batch}
     </div>
   );
 }
